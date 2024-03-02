2cdd3e394b2898723396a29d6af81695f3e6a9ca
Leonardo Lessi <LEONARDO.LESSI@local.ittcesena.it>
2024-03-02 11:43:40 +0100

    Modifica file sln

--- Codifiche_Ascii/Codifiche_Ascii/obj/Debug/net6.0/Codifiche_Ascii.AssemblyInfo.cs
+++ Codifiche_Ascii/Codifiche_Ascii/obj/Debug/net6.0/Codifiche_Ascii.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Codifiche_Ascii")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8cb966106170946815cad9da66295429ec279204")]
 [assembly: System.Reflection.AssemblyProductAttribute("Codifiche_Ascii")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Codifiche_Ascii")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

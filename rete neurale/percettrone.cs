78bf2589e22eb9c587a8b4c3e60db6259b9e8517
Leonardo Lessi <LEONARDO.LESSI@local.ittcesena.it>
2026-05-28 10:52:31 +0200

    percettrone v2

--- rete neurale/percettrone.cs
+++ rete neurale/percettrone.cs
@@ -2,61 +2,107 @@ using System;
 using System.IO;
 using System.Globalization;
 using System.Security.Cryptography.X509Certificates;
+using System.ComponentModel.Design.Serialization;
+using System.Reflection;
+using System.Diagnostics.CodeAnalysis;
 
-public static class PercettroneUtils
+namespace Percettrone
 {
-    public const int FEATURES = 5;
-    public const float THRESHOLD = 0.5f;
-
-    public static int Activation(float x)
+    internal class Program
     {
-        if (x > THRESHOLD)
-            return 1;
-        else
-            return 0;
-    }
+        const int domande = 5;
+        const double threshold = 0.5;
+        const double learning_rate = 0.1;
+        static double[] pesi = new double[domande] { 0.7, 0.6, 0.5, 0.3, 0.4 };
+        static double bias = 0.0;
+        
 
-    public static bool CaricaPesi(string filename, float[] weights, out float bias)
-    {
-        bias = 0.0f;
-
-        if (!File.Exists(filename))
+        static void Main(string[] args)
         {
-            Console.WriteLine($"Errore: file {filename} non trovato!");
-            return false;
-        }
+            Console.WriteLine("A: addestrare, P: usalo");
+            string scelta = Console.ReadLine();
 
-        try
-        {
-            string[] linee = File.ReadAllLines(filename);
-
-            for (int i = 0; i < FEATURES; i++)
+            if (scelta == "p")
             {
-                string valoreStr = linee[i].Substring(linee[i].IndexOf(':') + 1).Trim();
-                weights[i] = float.Parse(valoreStr, CultureInfo.CurrentCulture);
+                int[] input = new int[domande];
+                Console.WriteLine("Artista famoso? (1=si, 0=No):");
+                input[0] = int.Parse(Console.ReadLine());
+                Console.WriteLine("Bel meteo? (1=si, 0=No):");
+                input[1] = int.Parse(Console.ReadLine());
+                Console.WriteLine("Viene un amico? (1=si, 0=No):");
+                input[2] = int.Parse(Console.ReadLine());
+                Console.WriteLine("Cibo? (1=si, 0=No):");
+                input[3] = int.Parse(Console.ReadLine());
+                Console.WriteLine("Bere? (1=si, 0=No):");
+                input[4] = int.Parse(Console.ReadLine());
+
+                int decisione = prevedi(input);
+
+                if (decisione == 0)
+                {
+                    Console.WriteLine("Non andare");
+                }
+                else
+                {
+                    Console.WriteLine("Vai");
+                }
             }
+            else
+            {
+                StreamReader sr = new StreamReader("a.txt");
+                
+                int carattere;
 
-            string biasStr = linee[5].Substring(linee[5].IndexOf(':') + 1).Trim();
-            bias = float.Parse(biasStr, CultureInfo.CurrentCulture);
+                while (carattere = sr.Read() != -1)
+                {
+                    int[] input = new int[domande];
+                    input[0] = carattere - 48;
+                    input[1] = sr.Read() - 48;
+                    input[2] = sr.Read() - 48;
+                    input[3] = sr.Read() - 48;
+                    input[4] = sr.Read() - 48;
+                    int risosta = sr.Read() - 48;
 
-            return true;
+                    double somma = bias;
+
+                    for (int j = 0; j < domande; j++)
+                    {
+                        somma += input[j] * pesi[j];
+                    }
+
+                    int output = attivazione(somma);
+                    int error = risposta - output;
+
+                    for (int j = 0; j < domande; j++)
+                    {
+                        pesi[j] += learning_rate * error * input[j];
+                    }
+
+                    bias += learning_rate * error;
+                }
+
+                Console.WriteLine("pesi nuovi: " + pesi[0] + " " + pesi[1] + " " + pesi[2] + " " + pesi[3] + " " + pesi[4]);
+            }
         }
-        catch (Exception e)
+
+        static int attivazione(double x)
         {
-            Console.WriteLine($"Errore nella lettura del file: {ex.Message}");
-            return false;
+            if (x > threshold)
+            {
+                return 1;
+            }
+            else return 0;
         }
-
-    }
-    
-    public static int Prevedi(float[] weights, float bias, int[] input)
-    {
-        float somma = bias;
-        for (int i = 0; i > FEATURES; i++)
+        
+        static int prevedi (int[] input)
         {
-            somma += input[i] * weights[i];
-        }
-        return Activation(somma);
-    }
+            double somma = bias;
 
+            for (int i = 0; i < domande; i++)
+            {
+                somma += input[i] * pesi[i];
+            }
+            return attivazione(somma);
+        }
+    }
 }
